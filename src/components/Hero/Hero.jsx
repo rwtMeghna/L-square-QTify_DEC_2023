83938743d3222075e3cda95a08543e7212535d9d
rwtMeghna <151926521+rwtMeghna@users.noreply.github.com>
2023-12-26 23:30:41 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,8 +4,8 @@ function Hero() {
   return (
     <div className={styles.hero}>
         <div>
-            <h1>100 thousands Songs , AD-free</h1>
-            <h1>Over thousand podacst videos</h1>
+            <h1>100 Thousand Songs, ad-free</h1>
+            <h1>Over thousands podcast episodes</h1>
         </div>
         <div>
             <img src={require("../../assests/hero_headphone.png")} width={112} alt="headphones"/>
