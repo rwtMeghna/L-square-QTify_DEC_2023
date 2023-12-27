5d82b1a0767db7c27a574d496bdc705da597f3c8
rwtMeghna <151926521+rwtMeghna@users.noreply.github.com>
2023-12-27 23:25:50 +0530

    Fix line endings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React , {useState} from 'react';
+import React , {useEffect, useState} from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import { Outlet } from 'react-router-dom';
+import { fetchNewAlbums, fetchTopAlbums } from './Api/Api';
+// import { createBrowserRouter , RouterProvider } from 'react-router-dom';
 
 function App() {
   const [searchData , useSearchData]=useState();
-  return (<>
-    <Navbar searchData={searchData}/>
-    <Hero/>
+ const[data , setData]=useState({});
+
+ const generateData=(key , source)=>{
+  source().then((data)=>{
+    setData((prevData)=>{
+      return {...prevData , [key]:data};
+    });
+  });
+ };
+
+
+ useEffect(()=>{
+  generateData("topAlbums" , fetchTopAlbums);
+  generateData("newAlbums" , fetchNewAlbums);
+  //  generateData("songs" , fetchSongs);
+ } , []);
+
+const{ topAlbums=[] , newAlbums=[] , songs=[] }=data;
+
+  return ( 
+  <>
+  <div>
+    <Navbar searchData={[...topAlbums, ...newAlbums]} />
+    <Outlet context={{data: {topAlbums , newAlbums , songs} }}/>
+    </div>
     </>
   );
 }
+
 
 export default App;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,66 +1,62 @@
 import React from "react";
 import styles from "./Card.module.css";
-import {Tooltip} from "@mui/material";
-function Card({data , type}){
+import { Tooltip } from "@mui/material";
+import {Chip} from "@mui/material";
+import { Link } from "react-router-dom";
 
-    const getCard=(type)=>{
-        switch(type){
-            case "album":{
-                const {title , image , likes , follows, slug , songs}=data;
-
-                return(
-                <Tooltip title={`${songs.length}songs`} placement="top" arrow>
-                    <a href={`/album/${slug}`}>
-                <div className={styles.wrapper}>
-                    <div className={styles.card}>
-                        <img src={image} alt="song" loading="lazy"/>
-                        <div className={styles.banner}>
-                            <div className={styles.pill}>
-                                <p>
-                                    {likes}Likes
-                                </p>
+function Card({data, type }) {
+//   console.log(data);
+    const getCard = (type) => {
+        switch (type) {
+            case "album"    : {
+                const {image , title , follows , slug ,songs} = data;
+                return (
+                    <Tooltip title={`${songs.length} songs`} placement="top" arrow>
+                        <a href ={`/album/${slug}`}>
+                            <div className={styles.wrapper}>
+                                <div className={styles.card}>
+                                    <img src={image} alt="song" loading="lazy" />
+                                    <div className={styles.banner}>
+                                        <Chip 
+                                            label={`${follows} Follows`}
+                                            size="small"
+                                            className={styles.chip}
+                                            />
+                                    </div>
+                                </div>
+                                <div className={styles.titleWrapper}>
+                                    <p>{title}</p>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className={styles.titleWrapper}>
-                      <p>{title}</p>
-                    </div>
-                </div>
-                </a>
-                </Tooltip>
+                        </a>
+                    </Tooltip>
                 )
             }
 
-            case "song":{
-                const {title , image , likes}=data;
-
-                return(
-                <div className={styles.wrapper}>
-            
-            
-                    <div className={styles.card}>
-                        <img src={image} alt="song" loading="lazy"/>
-            
-                        <div className={styles.banner}>
-                            <div className={styles.pill}>
-                                <p>
-                                    {likes}Likes
-                                </p>
+            case "song": {
+                const {likes , image , title } = data;
+                return (
+                    <div className={styles.wrapper}>
+                        <div className={styles.card}>
+                            <img src={image} alt="song" loading="lazy" />
+                            <div className={styles.banner}>
+                                <div className={styles.pill}>
+                                    <p>{likes} Likes</p>
+                                </div>
                             </div>
                         </div>
+
+                        <div className={styles.titleWrapper}>
+                            <p>{title}</p>
+                        </div>
                     </div>
-                    
-                    <div className={styles.titleWrapper}>
-                      <p>{title}</p>
-                    </div>
-                </div>
                 )
             }
             default:
                 return <></>;
-            };
-        }
-        return getCard(type);
+        };
     }
+    return getCard(type);
+}
 
 export default Card;
